2d62240673d12e3c5d44908c6ed5b053ec17ccd3
Maxim Kirshin <meowto16@gmail.com>
2021-07-07 01:19:34 +0500

    Lesson 025. Practicing Components. Watched solution and removed unused components

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './alerts/warning-alert/warning-alert.component'
 import { SuccessAlertComponent } from './alerts/success-alert/success-alert.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
     WarningAlertComponent,
     SuccessAlertComponent,
   ],
