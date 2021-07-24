2cf9bf6b1dcd0565314cfb6ea2f5b4ed6b3ebeee
Maxim Kirshin <meowto16@gmail.com>
2021-07-24 19:05:18 +0500

    Lesson 115. Practicing Services. Completed

---
+++ projects/services-assignments-start/src/app/counter.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core'
+@Injectable({ providedIn: 'root' })
+export class CounterService {
+  activeToInactiveCounter = 0;
+  inactiveToActiveCounter = 0;
+
+  incrementActiveToInactive() {
+    this.activeToInactiveCounter++;
+    console.log(`Active to inactive counter: ${this.activeToInactiveCounter}`)
+  }
+
+  incrementInactiveToActive() {
+    this.inactiveToActiveCounter++;
+    console.log(`Inactive to active counter: ${this.inactiveToActiveCounter}`)
+  }
+}
