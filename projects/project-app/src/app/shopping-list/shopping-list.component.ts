86a19052ce575ff017ce255e4ebad371bc03960f
Maxim Kirshin <meowto16@gmail.com>
2021-07-25 06:45:06 +0500

    Lesson 117. Setting up the Services. Completed

--- projects/project-app/src/app/shopping-list/shopping-list.component.ts
+++ projects/project-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model'
+import { ShoppingListService } from './shopping-list.service'
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  providers: [ShoppingListService],
 })
 export class ShoppingListComponent {
   ingredients: Ingredient[]  = [
